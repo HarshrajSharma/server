85b1f45a1960cb7f0df4cb182861b01a5ecb712a
Harsh Sharma <harshrajsharma.2003@gmail.com>
2024-10-17 17:02:08 +0530

    Fixes

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const connectDB = require('./db/db.js')
 
 const homeRouter=require('./routes/Home.js');
 const userRouter = require('./routes/User.js');
-const config = require('./config/default.js');
+require('dotenv').config()
 
 const app = express();
 
@@ -28,5 +28,4 @@ app.use('/user', userRouter);
 const port=process.env.PORT || 5000;
 app.listen(port, ()=>{
     console.log('Server started at port: ', port);
-    console.log(config)
 })
